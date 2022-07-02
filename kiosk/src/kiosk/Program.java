18558acdeaf56004653b86b8a2e11c46826812c8
Gajun Choi <100130000+BrainHellChang@users.noreply.github.com>
2022-07-02 17:16:46 +0900

    상속을 활용한 카페 키오스크
    Menu 시작

---
+++ kiosk/src/kiosk/Program.java
@@ -0,0 +1,23 @@
+package kiosk;
+
+public class Program {
+
+   public static void main(String[] args) {
+      while(true) {
+
+
+         Screen sc = new Screen();
+         sc.showtit();    //tit 공차
+         sc.showbtn();    //btn 커피 음료 빵
+
+
+         NewScreen ns = new NewScreen();
+         ns.mat();       //메뉴판
+         ns.receipt();    //주문내역
+         ns.Buy();       // 결제 or주문취소
+
+
+      }
+
+   }   
+}  
